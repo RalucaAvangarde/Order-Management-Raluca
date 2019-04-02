9958c186040504bd963a292228c8249a0c4a0a97
RalucaAvangarde <raluca.metes@avangarde-software.com>
2019-04-02 16:52:33 +0300

    Json template , UI pop-up panels
    Json file template
    create canvas with 2 buttons and 2 pop-up panels

---
+++ Assets/Scripts/JsonUtils.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JsonUtils : MonoBehaviour
+{
+    public ElementsList DefaultElements { get; set; }
+    private string fileName = "Orders.json";
+    private string jsonFilePath;
+
+
+    private void Awake()
+    {
+        DefaultElements = new ElementsList();
+        jsonFilePath = Application.persistentDataPath + "/" + fileName;
+        Debug.Log(jsonFilePath);
+        SaveData();
+    }
+    private void SaveData()
+    {
+        string contents = JsonUtility.ToJson(DefaultElements, true);
+        File.WriteAllText(jsonFilePath, contents);
+    }
+    public void ReadData()
+    {
+
+        if (File.Exists(jsonFilePath))
+        {
+            string contents = File.ReadAllText(jsonFilePath);
+            DefaultElements = JsonUtility.FromJson<ElementsList>(contents);
+
+        }
+        else
+        {
+            Debug.Log("Unable to read default input file");
+            //SaveData();
+        }
+    }
+}
---
+++ Assets/Scripts/OrderData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class OrderData
+{
+    public ElementsList ManagementOrders;
+}
+
+[Serializable]
+public class ElementsList
+{
+    public List<Item> ElementList;
+    public List<Elements> ElementsOrder;
+}
+
+[Serializable]
+public class Item
+{
+    public int IdItem;
+    public int ItemQuantity;
+    public string ItemName;
+}
+[Serializable]
+public class Elements
+{
+    public int Order;
+    public string ClientName;
+}
